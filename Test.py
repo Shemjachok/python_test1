65fb1567f342300404d616a107035a9b6e4d5bde
Shemjachok <shmjak@yandex.ru>
2023-01-16 16:25:03 +0300

    Add file 1

--- Test.py
+++ Test.py
@@ -1,2 +1,8 @@
-print("Hello")
-print("Hello")
+money = int(input("Введите сумму вклада:"))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+for key in per_cent:
+    deposit.append(per_cent[key]*money/100)
+    print("deposit =" , deposit)
+max_deposit = max(deposit)
+print("Максимальная сумма, которую вы можете заработать:"+str(int(max_deposit)))
